69ec1b3937db96132c40896a1441bfa85bf500ce
Laz <lazdotdigital@gmail.com>
2021-11-26 16:46:36 -0800

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'reset-css/reset.css';
 import './index.css';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
 const root = document.querySelector('#root');
-ReactDOM.render(root);
+ReactDOM.render(<App />, root);
